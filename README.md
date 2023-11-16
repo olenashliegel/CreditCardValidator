6e613abf700ace0fab6de2eded5ff9bd8238d5f4
Olena Shliegel <olenashliegel@gmail.com>
2023-11-16 11:20:55 -0800

    It will add function and test for creditCardValidator function
    Co-authored-by: Ermek Abdrazakov <abdrazakovermek9@gmail.com>

--- README.md
+++ README.md
@@ -1,27 +1,48 @@
+Describe: splittingArray()
+
+Test1: "It should return array of numbers from text string"
+Code: 
+const inputTextTest1 = "4204070780465320"
+convertStringToNumberArray(inputTextTest1);
+Expected Output: [4,2,0,4,0,7,0,7,8,0,4,6,5,3,2,0]
+
+Test2: "It should return null if text contains any other symbols than numbers"
+Code: 
+const inputTextTest2 = "42a04070780465320";
+convertStringToNumberArray (inputTextTest2);
+Expected Output: null
+
+Test3: "It should return null in case of empty string"
+Code: 
+const inputTextTest3 = "";
+convertStringToNumberArray (inputTextTest3);
+Expected Output: null
+
+
 Describe: isCreditCardLengthCorrect()
 
-Test: "It should return true if length of array is 16 or 15 digits"
-Code: isCreditCardLengthCorrect();
-const textArray = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5, 3,2,0];
-creditCardLength(textArray);
+Test4: "It should return true if length of array is 16 digits"
+Code: 
+const numbersArrayTest4 = [4,2,0,4,0,7,0,7,8,0,4,6,5,3,2,0];
+isCreditCardLengthCorrect(numbersArrayTest4);
 Expected Output: true
 
-Test: "It should return true if length of array is 16 or 15 digits"
-Code: isCreditCardLengthCorrect();
-const textArray = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5, 3,2];
-creditCardLength(textArray);
+Test5: "It should return true if length of array is 15 digits"
+Code: 
+const numbersArrayTest5 = [4,2,0,4,0,7,0,7,8,0,4,6,5,3,2,0];
+isCreditCardLengthCorrect(numbersArrayTest5);
 Expected Output: true
 
-Test: "It should return false if length of array is NOT 16 or 15 digits"
-Code: isCreditCardLengthCorrect();
-const textArray = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5];
-creditCardLength(textArray);
+Test6.1: "It should return false if length of array is less than 16 or 15 digits"
+Code: 
+const numbersArrayTest6_1 = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5, 0];
+creditCardLength(numbersArrayTest6_1);
 Expected Output: false
 
-Test: "It should return false if length of array is NOT 16 or 15 digits"
-Code: isCreditCardLengthCorrect();
-const textArray = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5, 2, 2, 2, 2];
-creditCardLength(textArray);
+Test: "It should return false if length of array is more than 16 or 15 digits"
+Code: 
+const numbersArrayTest6_2 = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5, 2, 2, 2, 2];
+creditCardLength(numbersArrayTest6_2);
 Expected Output: false
 
 
@@ -46,3 +67,46 @@ Code: doublingEveryOtherDigit();
 const textArray =[18, 10, 11, 12, 13, 8, 0, 8, 8, 0, 4, 3, 5, 6, 2];
 creditCardLength(textArray);
 Expected Output: [9, 1, 2, 3, 4, 8, 0, 8, 8, 0, 4, 3, 5, 6, 2]
+
+Describe: validatingFirstDigits()
+Test: "It should return 'Visa card' if first element of array is equal 4"
+Code: 
+const numbersArrayTest4 = [4,2,0,4,0,7,0,7,8,0,4,6,5,3,2,0];
+convertStringToNumberArray(inputTextTest4);
+Expected Output: "Visa card"
+
+Test: "It should return 'Mastercard' if first element of array is equal 5"
+Code: 
+const numbersArrayTest4 = [5,2,0,4,0,7,0,7,8,0,4,6,5,3,2,0];
+convertStringToNumberArray(inputTextTest4);
+Expected Output: "Mastercard"
+
+Test: "It should return 'Discover card' if first element of array is equal 6"
+Code: 
+const numbersArrayTest4 = [6,2,0,4,0,7,0,7,8,0,4,6,5,3,2,0];
+convertStringToNumberArray(inputTextTest4);
+Expected Output: "Discover card"
+
+Test: "It should return 'American Express card' if first element of array is equal 3 AND the second element is equal 4"
+Code: 
+const numbersArrayTest4 = [3,4,0,4,0,7,0,7,8,0,4,6,5,3,2];
+convertStringToNumberArray(inputTextTest4);
+Expected Output: "American Express card"
+
+Test: "It should return 'American Express card' if first element of array is equal 3 AND the second element is equal 7"
+Code: 
+const numbersArrayTest4 = [3,7,0,4,0,7,0,7,8,0,4,6,5,3,2];
+convertStringToNumberArray(inputTextTest4);
+Expected Output: "American Express card"
+
+Test: "It should return NULL if first element of array is any of digits except of 4, 5, 6, 3 "
+Code: 
+const numbersArrayTest4 = [2,7,0,4,0,7,0,7,8,0,4,6,5,3,2];
+convertStringToNumberArray(inputTextTest4);
+Expected Output: "NULL"
+
+Test: "It should return NULL if first element of array is 3 and second element is NOT 4 or 7 "
+Code: 
+const numbersArrayTest4 = [3,6,0,4,0,7,0,7,8,0,4,6,5,3,2];
+convertStringToNumberArray(inputTextTest4);
+Expected Output: "NULL"
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+function convertStringToNumberArray(text) {
+  let nonNumericRegex = /\D/;
+  if (!nonNumericRegex.test(text) && (text.length > 0)) {
+    const splittingArray = text.split("");
+    const resultArray = splittingArray.map(function (element) {
+      return parseInt(element);
+    });
+    return resultArray;
+  } else
+    return null;
+}
+
 function isCreditCardLengthCorrect(creditCardArray) {
   if (creditCardArray.length === 16 || creditCardArray.length === 15) {
     return true;
@@ -10,7 +22,7 @@ function doublingEveryOtherDigit(creditCardArray) {
   let reversedArray = creditCardArray.reverse();
   let resultArray = [];
   resultArray = reversedArray.map(function (number, index) {
-    if (index % 2 === 0) {
+    if (index % 2 === 1) {
       return number * 2;
     } else {
       return number;
@@ -31,20 +43,82 @@ function addingDigitsOfDoubleDigitNumbers(creditCardArray) {
     else
       return element;
   });
-
   return resultArray;
 }
 
-// function creditCardValidator(creditCardArray) {
-// }
+function checkingArraySum(creditCardArray) {
+  let sum = 0;
+  creditCardArray.forEach(function (number) {
+    sum += number;
+  })
+  if (sum % 10 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-const array1 = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5, 3, 2, 0];
-const array2 = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5, 3, 2];
-const array3 = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5];
-const array4 = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5, 2, 2, 2, 2];
-const array5 = [4, 1, 0, 2, 0, 8, 0, 8, 8, 0, 4, 3, 5, 6, 2, 0];
-const array6 = [4, 1, 0, 2, 0, 8, 0, 8, 8, 0, 4, 3, 5, 6, 2];
-const array7 = [18, 10, 11, 12, 13, 8, 0, 8, 8, 0, 4, 3, 5, 6, 2];
+function validatingFirstDigits(creditCardArray) {
+  switch (creditCardArray[0]) {
+    case 4:
+      return "Visa card";
+    case 5:
+      return "Mastercard";
+    case 6:
+      return "Discover card";
+    case 3:
+      {
+        if (creditCardArray[1] === 4 || creditCardArray[1] === 7)
+          return "American Express card"
+        else
+          return null;
+      };
+    default:
+      return null;
+  }
+}
+
+function creditCardValidator(text) {
+  if (convertStringToNumberArray(text) != null) {
+    const numbersArray = convertStringToNumberArray(text);
+    if (isCreditCardLengthCorrect(numbersArray)) {
+      return checkingArraySum(addingDigitsOfDoubleDigitNumbers(doublingEveryOtherDigit(numbersArray)));
+    } else
+      return false;
+  } else
+    return false;
+}
+
+
+// const array3 = [3, 9, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5];
+// //console.log(validatingFirstDigits(array3));
+
+// const array4 = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5, 2, 2, 2, 2];
+// const array5 = [4, 1, 0, 2, 0, 8, 0, 8, 8, 0, 4, 3, 5, 6, 2, 0];
+// const array6 = [4, 1, 0, 2, 0, 8, 0, 8, 8, 0, 4, 3, 5, 6, 2];
+// const array7 = [4, 1, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0];
+// const array8 = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5, 3, 2, 0];
+
+// const inputTextTest1 = "4204070780465320";
+// console.log(convertStringToNumberArray(inputTextTest1));
+
+// const inputTextTest2 = "42a04070780465320";
+// console.log(convertStringToNumberArray(inputTextTest2));
+
+// const inputTextTest3 = "";
+// console.log(convertStringToNumberArray(inputTextTest3));
+
+// const numbersArrayTest4 = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5, 3, 2, 0];
+// console.log(isCreditCardLengthCorrect(numbersArrayTest4));
+
+// const numbersArrayTest5 = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5, 3, 2];
+// console.log(isCreditCardLengthCorrect(numbersArrayTest5));
+
+// const numbersArrayTest6_1 = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5, 0];
+// console.log(isCreditCardLengthCorrect(numbersArrayTest6_1));
+
+// const numbersArrayTest6_2 = [4, 2, 0, 4, 0, 7, 0, 7, 8, 0, 4, 6, 5, 2, 2, 2, 2];
+// console.log(isCreditCardLengthCorrect(numbersArrayTest6_2));
 
 // console.log(isCreditCardLengthCorrect(array1));
 // console.log(isCreditCardLengthCorrect(array2));
@@ -53,4 +127,25 @@ const array7 = [18, 10, 11, 12, 13, 8, 0, 8, 8, 0, 4, 3, 5, 6, 2];
 
 //console.log(doublingEveryOtherDigit(array5));
 //console.log(doublingEveryOtherDigit(array6));
-console.log(addingDigitsOfDoubleDigitNumbers(array7));
+// console.log(addingDigitsOfDoubleDigitNumbers(array7));
+
+// console.log(creditCardValidator("378282246310005"));
+// console.log(creditCardValidator("371449635398431"));
+// console.log(creditCardValidator("378734493671000"));
+// console.log(creditCardValidator("5499740000000057"));
+// console.log(creditCardValidator("5555555555554444"));
+// console.log(creditCardValidator("5105105105105100"));
+
+// console.log(creditCardValidator("6011000991001201"));
+// console.log(creditCardValidator("6011111111111117"));
+// console.log(creditCardValidator("6011000990139424"));
+
+// console.log(creditCardValidator("4111111111111111"));
+// console.log(creditCardValidator("4242424242424242"));
+//console.log(creditCardValidator("4012888888881881"));
+//console.log(creditCardValidator("4222222222222"));
+
+// console.log(creditCardValidator("5105105105105100"));
+// console.log(creditCardValidator("420a4070780465320"));
+
+
